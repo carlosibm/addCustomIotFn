524b5bd5b49799f6e80b943c42f70d9694c03594
Ankit Jha <jhaa@us.ibm.com>
2020-02-06 13:46:23 -0600

    Adding required imports

--- extremeanomaly/extremeanomalygenerator.py
+++ extremeanomaly/extremeanomalygenerator.py
@@ -1,6 +1,7 @@
 import logging
 import numpy as np
 import pandas as pd
+import datetime
 
 from iotfunctions.base import BaseTransformer
 from iotfunctions.ui import (UISingle, UIFunctionOutSingle, UISingleItem)

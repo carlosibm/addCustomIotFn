20bdc12034105c92b6c13c65d82a4f7100ee562c
Ankit Jha <jhaa@us.ibm.com>
2020-02-14 10:53:19 -0600

    Adding loggers

--- scripts/register_my_custom_function.py
+++ scripts/register_my_custom_function.py
@@ -14,7 +14,7 @@ with open('credentials_as.json', encoding='utf-8') as F:
 db_schema = None
 db = Database(credentials=credentials)
 
-from customaj.multiplybyfactoraj import MultiplyByFactorAJ
+from flatlineanomalyaj.flatlineanomalygeneratoraj.py import FlatlineAnomalyGenerator
 
-db.register_functions([MultiplyByFactorAJ])
+db.register_functions([FlatlineAnomalyGenerator])
 
